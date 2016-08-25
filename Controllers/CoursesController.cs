9dd272405f92e7492604bc827003ade6de2c6973
Hauxii <haukur91@gmail.com>
2016-08-25 15:08:17 +0000

    update

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication.Models;
 
-
 namespace WebApplication.Controllers
 {
     [Route("/api/courses")]
     public class CoursesController : Controller
     {
-        public List<Course> GetCourses(){
-            return new List<Course>{
+
+        public List<Course> allCourses = new List<Course>{
                 new Course {
                     Name = "Web Services",
                     TemplateID = "T-514-VEFT",
@@ -25,7 +24,35 @@ namespace WebApplication.Controllers
                     StartDate = new DateTime(2016, 08, 17),
                     EndDate = new DateTime(2016-11-08)
                 }
-            }; 
+        };
+
+        public List<Course> GetCourses()
+        {
+            return allCourses;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetCourseByID(int id)
+        {
+            Course c = null;
+            for(int i = 0; i < allCourses.Count; i++){
+                if(allCourses[i].ID == id){
+                    c = allCourses[i];
+                }
+            }
+            if(c == null){
+                return StatusCode(404);
+            }
+            return Ok(c);
+        }
+
+        [HttpPost]
+        [Route("{id}")]
+        public IActionResult PostCourse(Course c)
+        {
+            allCourses.Add(c);
+            return StatusCode(201);
         }
     
     }
