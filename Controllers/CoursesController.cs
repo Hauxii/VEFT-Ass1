99de60ca2cec542f76d678e918d231fe175ebab4
Hauxii <haukur91@gmail.com>
2016-08-26 09:09:04 +0000

    help me plz

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -8,23 +8,27 @@ namespace WebApplication.Controllers
     [Route("/api/courses")]
     public class CoursesController : Controller
     {
-
-        public List<Course> allCourses = new List<Course>{
-                new Course {
-                    Name = "Web Services",
-                    TemplateID = "T-514-VEFT",
-                    ID = 1,
-                    StartDate = new DateTime(2016, 08, 17),
-                    EndDate = new DateTime(2016-11-08)
-                },
-                new Course {
-                    Name = "Honnun og smidi",
-                    TemplateID = "T-302-HONN",
-                    ID = 2,
-                    StartDate = new DateTime(2016, 08, 17),
-                    EndDate = new DateTime(2016-11-08)
-                }
-        };
+        public List<Course> allCourses;
+        public CoursesController(){
+            if(allCourses == null){
+                allCourses = new List<Course>{
+                    new Course {
+                        Name = "Web Services",
+                        TemplateID = "T-514-VEFT",
+                        ID = 1,
+                        StartDate = new DateTime(2016, 08, 17),
+                        EndDate = new DateTime(2016-11-08)
+                    },
+                    new Course {
+                        Name = "Honnun og smidi",
+                        TemplateID = "T-302-HONN",
+                        ID = 2,
+                        StartDate = new DateTime(2016, 08, 17),
+                        EndDate = new DateTime(2016-11-08)
+                    }
+                };
+            }
+        }
 
         public List<Course> GetCourses()
         {
@@ -32,27 +36,36 @@ namespace WebApplication.Controllers
         }
 
         [HttpGet]
-        [Route("{id}")]
+        [Route("{id:int}", Name = "GetCourseByID")]
         public IActionResult GetCourseByID(int id)
         {
-            Course c = null;
-            for(int i = 0; i < allCourses.Count; i++){
-                if(allCourses[i].ID == id){
-                    c = allCourses[i];
-                }
-            }
-            if(c == null){
+            Course toFind = allCourses.Find(item => item.ID == id);
+
+            if(toFind == null){
                 return StatusCode(404);
             }
-            return Ok(c);
+            return Ok(toFind);
         }
 
         [HttpPost]
-        [Route("{id}")]
-        public IActionResult PostCourse(Course c)
+        public IActionResult CreateCourse([FromBody]dynamic newCourse)
         {
-            allCourses.Add(c);
-            return StatusCode(201);
+            /*
+            Course newCourse = new Course {
+                ID = data.id,
+                TemplateID = data.templateid,
+                Name = data.name,
+                StartDate = data.startdate,
+                EndDate = data.enddate
+            };
+            */
+            
+            return newCourse;
+            /*
+            allCourses.Add(newCourse);
+            var location = Url.Link("GetCourseByID", new { id = newCourse.ID});
+            return Created(location, newCourse);
+            */
         }
     
     }
